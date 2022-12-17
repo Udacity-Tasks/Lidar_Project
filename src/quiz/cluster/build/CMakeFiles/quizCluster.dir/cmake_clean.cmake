ef0d4cb09dcc54194972872db579b6eba621ac6e
Muzaffer Citir <mcitir_linux@MSI-ubuntu-20.04>
2022-12-17 21:28:25 +0100

    initial

---
+++ src/quiz/cluster/build/CMakeFiles/quizCluster.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quizCluster.dir/cluster.cpp.o"
+  "CMakeFiles/quizCluster.dir/media/mcitir_linux/Storage/Projects/COURSES/Udacity/Sensor_Fusion/01-Lidar/01-Intro/SFND_Lidar_Obstacle_Detection/src/render/render.cpp.o"
+  "quizCluster"
+  "quizCluster.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quizCluster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
